ca33117b154f3af43668335df9033b2e61033d73
Red Fox <link.to.my.email@gmail.com>
2022-11-15 21:09:19 +0300

    before 'read related data' step

--- PathwayUniversity/Models/Enrollment.cs
+++ PathwayUniversity/Models/Enrollment.cs
@@ -1,4 +1,6 @@
 ﻿using PathwayUniversity.Models;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace PathwayUniversity.Models
 {
@@ -12,6 +14,7 @@ namespace PathwayUniversity.Models
         public int EnrollmentID { get; set; }
         public int CourseID { get; set; }
         public int StudentID { get; set; }
+        [DisplayFormat(NullDisplayText = "No grade")]
         public Grade? Grade { get; set; }
 
         public Course Course { get; set; }
