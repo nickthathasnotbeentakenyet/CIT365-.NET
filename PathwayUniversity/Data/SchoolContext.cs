edfea8e5e3b62befa7fc84b455b94f15ed0a81fc
Red Fox <link.to.my.email@gmail.com>
2022-11-15 18:53:32 +0300

    initial setup

---
+++ PathwayUniversity/Data/SchoolContext.cs
@@ -0,0 +1,24 @@
+﻿using PathwayUniversity.Models;
+using Microsoft.EntityFrameworkCore;
+using PathwayUniversity.Models;
+using System.Collections.Generic;
+
+namespace PathwayUniversity.Data
+{
+    public class SchoolContext : DbContext
+    {
+        public SchoolContext(DbContextOptions<SchoolContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Course> Courses { get; set; }
+        public DbSet<Enrollment> Enrollments { get; set; }
+        public DbSet<Student> Students { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Course>().ToTable("Course");
+            modelBuilder.Entity<Enrollment>().ToTable("Enrollment");
+            modelBuilder.Entity<Student>().ToTable("Student");
+        }
+    }
+}
---
+++ PathwayUniversity/Models/Course.cs
@@ -0,0 +1,16 @@
+﻿using PathwayUniversity.Models;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace PathwayUniversity.Models
+{
+    public class Course
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
+        public int CourseID { get; set; }
+        public string Title { get; set; }
+        public int Credits { get; set; }
+
+        public ICollection<Enrollment> Enrollments { get; set; }
+    }
+}
