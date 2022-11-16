2e6db736124121c2f9ccf31b4624bfba5619bc9d
Red Fox <link.to.my.email@gmail.com>
2022-11-16 15:09:29 +0300

    handle concurrency conflicts DONE

--- PathwayUniversity/Migrations/20221116114635_RowVersion.Designer.cs
+++ PathwayUniversity/Migrations/20221116114635_RowVersion.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using PathwayUniversity.Data;
 
 namespace PathwayUniversity.Migrations
 {
     [DbContext(typeof(SchoolContext))]
-    partial class SchoolContextModelSnapshot : ModelSnapshot
+    [Migration("20221116114635_RowVersion")]
+    partial class RowVersion
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
